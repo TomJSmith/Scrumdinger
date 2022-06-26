be203423a0d117f6691807fbab0dd479aa9577e6
Thomas Smith <thomas_smith@live.ca>
2022-06-26 11:18:12 -0600

    Displaying Data in a List

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Tom on 2022-06-26.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
